2b65c5029615ec6a6d5737d1de27bdc877c9c3ab
andrei_labadaev <andrei-labadaev@mail.ru>
2021-10-24 20:58:18 +0300

    uppdate 20.58

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,6 @@
 //= components/slick.min.js
+//= components/jquery.timepicker.min.js
+//= components/datepicker.min.js
 
 $('.slaider__one').slick({
     infinite: true,
@@ -22,4 +24,13 @@ $('.slaider__one').slick({
         }
 
     ]
+});
+
+// input time
+$('.time').timepicker({
+    timeFormat: 'H:mm',
+    interval: 1,
+    minTime: "7",
+    maxTime: '20',
+
 });
