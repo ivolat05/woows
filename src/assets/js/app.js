2c40249813019e82009dfd717486e693f39f902d
andrei_labadaev <andrei-labadaev@mail.ru>
2021-10-25 21:37:28 +0300

    uppdate 21.40

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 //= components/slick.min.js
 //= components/jquery.timepicker.min.js
 //= components/datepicker.min.js
+//= components/jquery.magnific-popup.js
 
 // slaiders
 $('.slaider__one').slick({
@@ -58,4 +59,16 @@ $('.time').timepicker({
     minTime: "7",
     maxTime: '20',
 
+});
+
+// gallaty img
+$('.catalog__box-img').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Загрузка изображения #%curr%...',
+    gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    }
 });
